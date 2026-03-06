cb717141dd77e628389ea340af54b026d53b9be0
philippele1234-source <philippele1234@gmail.com>
2026-03-06 18:19:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+function createWindow(){
+
+const win = new BrowserWindow({
+width:1200,
+height:700,
+icon:path.join(__dirname,"assets/icon.png"),
+webPreferences:{
+preload:path.join(__dirname,"preload.js"),
+nodeIntegration:false,
+contextIsolation:true
+}
+})
+
+win.loadFile("index.html")
+
+}
+
+app.whenReady().then(()=>{
+createWindow()
+
+app.on("activate",()=>{
+if(BrowserWindow.getAllWindows().length===0){
+createWindow()
+}
+})
+
+})
+
+app.on("window-all-closed",()=>{
+if(process.platform!=="darwin"){
+app.quit()
+}
+})
---
+++ preload.js
@@ -0,0 +1,3 @@
+window.addEventListener("DOMContentLoaded", () => {
+console.log("VoidWare V10 Loaded")
+})
